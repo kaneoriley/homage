a7bbf9f585d6b7c6295bf5e9762913bfb2863ed6
Kane O'Riley <kane@oriley.me>
2016-04-13 19:43:28 +0930

    Add splash screen to sample app

--- homage-sample/src/main/java/me/oriley/homagesample/MainActivity.java
+++ homage-sample/src/main/java/me/oriley/homagesample/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
